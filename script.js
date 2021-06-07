0298dcd0fcbfe8d0646a3b752530fb3b6638b0d6
akyravish <info.akshayravish@gmail.com>
2021-06-07 15:14:40 +0530

    add js lines for the auto text project

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const text = document.getElementById('text');
+const input = document.getElementById('speed');
+const showText = 'We all love Programming.';
 
+let startingText = 1;
+let speed = 500;
+
+const autoText = () => {
+	text.innerText = showText.slice(0, startingText);
+	startingText++;
+
+	if (startingText > showText.length) {
+		startingText = 1;
+	}
+	setTimeout(() => {
+		autoText();
+	}, speed);
+};
+
+autoText();
+
+input.addEventListener('input', (e) => {
+	speed = 500 / e.target.value;
+});
